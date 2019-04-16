1f9899d79097238cc9061ca4051814050b3d1e1b
Anupam Godse <angodse@eb2-2236-aw07.csc.ncsu.edu>
2019-04-16 16:43:37 -0400

    pesudo code for client, todo locking

--- client.py
+++ client.py
@@ -4,6 +4,19 @@ import sys
 DATA=0
 ACK=1
 PACKET_TYPES={DATA:'0101010101010101', ACK:'1010101010101010'}
+SERVER_NAME = ''
+SERVER_PORT = ''
+FILENAME = ''
+WINDOW_SIZE = 0
+MSS = 1400
+Sn = 0
+Sf = 0
+SEQ_NO = 0
+FRAME_STORE = []
+
+RTO = 0.5
+
+timer = signal.ITIMER_REAL
 
 class Header:
     def __init__(self, seq_no, checksum, packet_type):
@@ -31,29 +44,54 @@ class Frame:
 
 def recv_acks():
     print("I am receiver")
+    global sock
+    while(True):
+    	ack = sock.recv(4096)
+    	if(currupted(ack)):
+		continue;
+	
+	if(ack_no > Sf and ack_no <= Sn):
+		for i in range(ack_no-Sf):
+			FRAME_STORE.pop(0)
+		signal.setitimer(timer, 0)
+	
 
 
-#class Window:
-#    def __init__(self, size):
-#        self.size = size
-#        self.sn = 0;
-#        self.sf = 0;
-#        self.filled=0;
-#        self.frames = []
-#        for each in range(size):
-#            self.frames.append(Frame())
+def getchecksum(data):
+	return 100;
+
+
+def storeframe(frame):
+	while(len(FRAME_STORE) >= WINDOW_SIZE):
+		sleep(1);
+	
+	FRAME_STORE.append(frame)
+
+def sendframe(sock, frame):
+	frame_data = frame.getframe()
+	sock.sendall(frame_data)
+
+def timeout(signum, frame):
+	signal.setitimer(timer, RTO)
+	for each in FRAME_STORE:
+		sendframe(sock, frame)
+	
         
 if __name__ == '__main__':
     #w = Window(4)
     
-    server_name = sys.argv[1]
-    server_port = int(sys.argv[2])
-    filename = sys.argv[3]
-    window_size = int(sys.argv[4])
+    SEVER_NAME = sys.argv[1]
+    SERVER_PORT = int(sys.argv[2])
+    FILENAME = sys.argv[3]
+    WINDOW_SIZE = int(sys.argv[4])
     MSS = int(sys.argv[5])
 
-    sn = 0;
-    sf = 0;
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+    server_ip = gethostbyname(SEVER_NAME)
+    
+    sock.connect((server_ip, SEVER_PORT))
+    
+    signal.signal(signal.SIGALRM, timeout)
 
     r = threading.Thread(target=receiver)
 
@@ -63,8 +101,19 @@ if __name__ == '__main__':
     data_size = len(data)
 
     while(data_size > 0):
-        while(sn-sf>=window_size):
+        #while(sn-sf>=window_size):
+	frame_data = SEQ_NO*MSS:(SEQ_NO+1)*MSS]
+	checksum = getchecksum(frame_data)
+	header = Header(SEQ_NO, checksum, DATA);
+	frame = Frame(header, frame_data)
+	sendframe(sock, frame)
+	SEQ_NO += 1
 
+	if(signal.getitimer(timer) == 0):
+		signal.setitimer(timer, RTO)
+	
+	
+	
 
     
 
